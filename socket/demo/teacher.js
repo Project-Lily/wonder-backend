d0ea6db3dfcfdbde9af45dfd63c03ce224a78b24
Aric-prog <hernandoaric@gmail.com>
2023-03-24 10:13:47 +1100

    changed teacher demo to show reconnect

--- socket/demo/teacher.js
+++ socket/demo/teacher.js
@@ -1,13 +1,22 @@
 // Demo example for a teacher connection
-ws = new WebSocket("wss://lilly.arichernando.com")
-ws.onopen = (data) => {
-    ws.onmessage = (data) =>{
-        console.log(`My id is : ${data.data}`)
-        // Creates room and destroys own listener, this is so that this listener doesn't mess with future events
-        createRoom(data.data)
+
+function createNewWs() {
+    ws = new WebSocket("wss://lilly.arichernando.com")
+    ws.onopen = (data) => {
+        ws.onmessage = (data) =>{
+            console.log(`My id is : ${data.data}`)
+            // Creates room and destroys own listener, this is so that this listener doesn't mess with future events
+            createRoom(data.data)
+        }
+        console.log("Connected")
     }
-    console.log("Connected")
+
+    ws.onclose = (data) => {
+        console.log("Connection closed :c")
+        createNewWs()
+    }
 }
+
 
 function createRoom(data) {
     // MAKE SURE IT'S HTTPS, nginx is cursed
